a56cd9864a4f7d7837f2b40222a6533070ddcd8a
Aarsh Bharti <aarshbharti.2021cs@technonjr.org>
2024-04-12 23:25:09 +0530

    Initial Commit

---
+++ day-012/src/App.jsx
@@ -0,0 +1,54 @@
+import './App.css';
+import { useState } from 'react';
+
+
+
+
+function App() {
+  
+const [todoList , setTodoList] = useState([]);
+const [newTask , setNewTask] = useState("");
+
+const handleChange = (events) => {
+setNewTask(events.target.value);
+}
+
+const addTask = () => {
+  setTodoList([...todoList , newTask]);
+}
+
+
+const add1 = ()=>{
+
+}
+
+const deleteTask = (taskName) => {
+
+setTodoList(todoList.filter((task) => task !== taskName));
+}
+
+
+  
+  return (
+    <div className="App">
+
+      <div className='addTask'> 
+      <input onChange={handleChange} />
+      <button onClick={addTask}> Add task </button>
+      </div>
+ 
+      <div className='list'>
+      {todoList.map((task) => {
+        return <div> 
+          <h1> {task} </h1> 
+          <button onClick={()=> deleteTask(task)}> X </button>
+          </div>
+      })}
+      </div>
+
+    </div>
+  );
+}
+
+
+export default App;
