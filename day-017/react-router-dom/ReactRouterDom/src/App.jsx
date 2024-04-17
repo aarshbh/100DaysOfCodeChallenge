ec2e4f01d4023d4674b9b8e2d825da15204e9111
Aarsh Bharti <aarshbharti.2021cs@technonjr.org>
2024-04-17 22:24:26 +0530

    Initial Commit

---
+++ day-017/react-router-dom/ReactRouterDom/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+import {BrowserRouter as Router , Routes , Route} from 'react-router-dom';
+import {Home} from "./Pages/Home";
+import {Contact} from "./Pages/Contact";
+import {Menu} from "./Pages/Menu";
+
+
+function App() {
+  
+
+  return (
+    <div className='App'>
+      <Router>
+        <Routes>
+
+          <Route path="/Home" element={<Home/>}/>
+          <Route path="/Menu" element={<Menu/>}/>
+          <Route path="/Contact" element={<Contact/>}/>
+
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ day-017/react-router-dom/ReactRouterDom/src/Pages/Menu.jsx
@@ -0,0 +1,5 @@
+const Menu = () => {
+    return <h1>This is Menu Page</h1>;
+};
+
+export default Menu;
