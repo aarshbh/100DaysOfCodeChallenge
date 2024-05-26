3586cc4d362306c4c01c6b780d7fef1b9d50b520
Aarsh Bharti <103813892+aarshbh@users.noreply.github.com>
2024-05-26 23:29:25 +0530

    Update Firebase.ts

--- day-045/FireBase/src/Config/Firebase.ts
+++ day-045/FireBase/src/Config/Firebase.ts
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD4a5QSJKiMi-7ckan2vhbt7jkMTgHrwP4",
-  authDomain: "fir-01-942fc.firebaseapp.com",
-  projectId: "fir-01-942fc",
-  storageBucket: "fir-01-942fc.appspot.com",
-  messagingSenderId: "900354992052",
-  appId: "1:900354992052:web:f7f4ec07ce108ed5a434e1",
-  measurementId: "G-G2QJLM5K5T"
+  <<NULL ASK THE OPERATOR>>
 };
 
 // Initialize Firebase
