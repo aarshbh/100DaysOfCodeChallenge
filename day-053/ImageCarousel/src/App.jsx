69b413c332f8af72372ea65023b9d932101d010c
Aarsh Bharti <aarshbharti.2021cs@technonjr.org>
2024-07-04 23:49:11 +0530

    Initial Commit

---
+++ day-053/ImageCarousel/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Carousel from "./Components/Carousel";
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      <Carousel />
+    </div>
+  );
+}
+
+export default App;
---
+++ day-053/ImageCarousel/src/Helpers/CarouselData.jsx
@@ -0,0 +1,17 @@
+import delhi from "../images/delhi.jpeg";
+import mumbai from "../images/mumbai.jpeg";
+import UDAIPUR from "../images/UDAIPUR.jpg";
+
+export const images = [
+  { title: "Delhi", subtitle: "This is Delhi", img: delhi },
+  {
+    title: "Mumbai, Maharashtra",
+    subtitle: "The Best City in the World",
+    img: mumbai,
+  },
+  {
+    title: "UDAIPUR (LAKE CITY)",
+    subtitle: "Khama Ganni",
+    img: UDAIPUR,
+  },
+];
