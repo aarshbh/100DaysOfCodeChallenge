fdac764288332f480d3480f151c6f88cda99ba0b
Aarsh Bharti <aarshbharti.2021cs@technonjr.org>
2024-07-12 06:00:16 +0530

    FireBase- Authentication - App

---
+++ day-060/React_Firebase_Authentication/src/Components/Interface.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Interface = () => {
+  return (
+    <div>
+      <div>
+        <input type='text'></input>
+      </div>
+    </div>
+  )
+}
+
+export default Interface
