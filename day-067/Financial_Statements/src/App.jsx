57d18d2e6fb3f9fb890bb37b47f1e617e42431e7
Aarsh Bharti <aarshbharti.2021cs@technonjr.org>
2024-07-18 14:10:06 +0530

    Penny&Dian Financial Management Application.

---
+++ day-067/Financial_Statements/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1> Financial Statement Tracker</h1>
+    </div>
+  )
+}
+
+export default App
