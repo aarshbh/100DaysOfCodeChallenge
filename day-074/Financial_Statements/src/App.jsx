dd5364a5748582e072f895e0f1dce4938b729076
Aarsh Bharti <aarshbharti.2021cs@technonjr.org>
2024-07-25 11:01:29 +0530

    Penny&Dian Manage your finance (Completed)

---
+++ day-074/Financial_Statements/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import {BrowserRouter as Router , Routes , Route } from "react-router-dom"
+import {Auth} from "./Pages/Auth/indexAuth";
+import {ExpenseTracker} from "./Pages/Expense-Tracker/indexExpense";
+function App() {
+  
+
+  return (
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/' exact element={<Auth />} />
+          <Route path='/expense-tracker' exact element={<ExpenseTracker />} />
+
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
